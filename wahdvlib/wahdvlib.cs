f1d84d83b1cb0130bdee9db68148f6f4bd37e69e
Ralph <kingiii003@gmail.com>
2013-04-28 01:29:15 +0800

    add log4net

--- wahdvlib/wahdvlib.cs
+++ wahdvlib/wahdvlib.cs
@@ -8,13 +8,16 @@ using System.Net;
 using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json;
 
+using log4net;
+
 namespace wahdvlib
 {
     public class wahdvlib
     {
         private static wahdvlib _instance = null;
-        private long lngLastModified = 0;
-        private string strRealm; 
+        private UInt64 lngLastModified = 0;
+        private string strRealm;
+        private LogManager logger;
 
         protected wahdvlib(string strRealmName)
         {
@@ -30,20 +33,40 @@ namespace wahdvlib
             return _instance;
         }
 
-        public bool checkModified()
+        public bool isModified()
         {
             HttpWebRequest rqstLastModified = (HttpWebRequest)WebRequest.Create("http://us.battle.net/api/wow/auction/data/" + strRealm);
             HttpWebResponse rspLastModified = (HttpWebResponse)rqstLastModified.GetResponse();
             if (rspLastModified.StatusCode == HttpStatusCode.OK)
             {
-                Stream streamResponse = rspLastModified.GetResponseStream();
-                StreamReader reader = new StreamReader(streamResponse, Encoding.GetEncoding(rspLastModified.ContentEncoding));
-                string strLastModified = reader.ReadToEnd();
-                
+                try
+                {
+                    Stream streamResponse = rspLastModified.GetResponseStream();
+                    StreamReader reader = new StreamReader(streamResponse, Encoding.GetEncoding(rspLastModified.ContentEncoding));
+                    string strLastModified = reader.ReadToEnd();
+                    realm CurrentRealm = JsonConvert.DeserializeObject<realm>(strLastModified);
+                    if (CurrentRealm.LastModified != lngLastModified)
+                    {
+                        lngLastModified = CurrentRealm.LastModified;
+                        return true;
+                    }
+                }
+                catch (JsonException JsonEx)
+                {
+                	
+                }
+                return false;
             }
             else return false;
         }
 
+    }
+
+
+    public class realm
+    {
+        public UInt64 LastModified { get; set; }
+        public string url { get; set; }
     }
 
 }
