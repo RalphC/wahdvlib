ef57ad296e1f04ca79305a4da55c813e210bf220
Ralph <kingiii003@gmail.com>
2013-04-27 17:01:08 +0800

    check LastModified Tag

--- wahdvlib/wahdvlib.cs
+++ wahdvlib/wahdvlib.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.IO;
 using System.Net;
 
+using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json;
 
 namespace wahdvlib
@@ -12,35 +13,37 @@ namespace wahdvlib
     public class wahdvlib
     {
         private static wahdvlib _instance = null;
-        //protected static bool isInitialized = false;
+        private long lngLastModified = 0;
+        private string strRealm; 
 
-        protected wahdvlib()
+        protected wahdvlib(string strRealmName)
         {
-
+            strRealm = strRealmName;
         }
 
-        public static wahdvlib getInstance()
+        public static wahdvlib getInstance(string Realm)
         {
             if (_instance == null)
             {
-                _instance = new wahdvlib();
+                _instance = new wahdvlib(Realm);
             }
             return _instance;
         }
 
+        public bool checkModified()
+        {
+            HttpWebRequest rqstLastModified = (HttpWebRequest)WebRequest.Create("http://us.battle.net/api/wow/auction/data/" + strRealm);
+            HttpWebResponse rspLastModified = (HttpWebResponse)rqstLastModified.GetResponse();
+            if (rspLastModified.StatusCode == HttpStatusCode.OK)
+            {
+                Stream streamResponse = rspLastModified.GetResponseStream();
+                StreamReader reader = new StreamReader(streamResponse, Encoding.GetEncoding(rspLastModified.ContentEncoding));
+                string strLastModified = reader.ReadToEnd();
+                
+            }
+            else return false;
+        }
+
     }
 
-
-    public class downloader
-    {
-        HttpWebRequest request;
-        HttpWebResponse response;
-        Stream requestStream;
-        Stream responseStream;
-
-        public void run()
-        {
-            
-        }
-    } 
 }
