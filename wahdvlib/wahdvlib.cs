656b0a0782ff6bd01d0d137290fd43aa3963d277
Ralph <kingiii003@gmail.com>
2013-04-28 16:51:52 +0800

    deserize done, to be testted

--- wahdvlib/wahdvlib.cs
+++ wahdvlib/wahdvlib.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 
 using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json;
@@ -12,12 +13,56 @@ using log4net;
 
 namespace wahdvlib
 {
+    private class dumpInfo
+    {
+        public UInt64 LastModified { get; set; }
+        public string url { get; set; }
+    }
+
+    private class auctionData
+    {
+        public realmInfo realm {get; set;}
+        public auction alliance {get; set;}
+        public auction horde {get; set;}
+        public auction neutral {get; set;}
+    }
+
+    private class realmInfo
+    {
+        public string name {get; set;}
+        public string slug {get; set;}
+    }
+
+    private class auction
+    {
+        public List<auctionItem> auctions {get; set;}
+    }
+
+    private class auctionItem
+    {
+        public UInt64 auc {get; set;}
+        public UInt32 item {get; set;}
+        public string owner {get; set;}
+        public UInt64 bid {get; set;}
+        public UInt64 buyout {get; set;}
+        public UInt32 quality {get; set;}
+        public string timeleft {get; set;}
+        public Int32 rand {get; set;}
+        public UInt64 seed {get; set;}
+        public UInt32 petSpeciesId {get; set;}
+        public UInt32 petBreedId {get; set;}
+        public UInt16 petLevel {get; set;}
+        public UInt16 petQualityId {get; set;}
+    }
+
     public class wahdvlib
     {
         private static wahdvlib _instance = null;
+        private auctionData currentData;
         private UInt64 lngLastModified = 0;
+        private string strAuctionData = "";
         private string strRealm;
-        private LogManager logger;
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         protected wahdvlib(string strRealmName)
         {
@@ -35,7 +80,7 @@ namespace wahdvlib
 
         public bool isModified()
         {
-            HttpWebRequest rqstLastModified = (HttpWebRequest)WebRequest.Create("http://us.battle.net/api/wow/auction/data/" + strRealm);
+            HttpWebRequest rqstLastModified = (HttpWebRequest)WebRequest.Create("http://www.battlenet.com.cn/api/wow/auction/data/" + strRealm);
             HttpWebResponse rspLastModified = (HttpWebResponse)rqstLastModified.GetResponse();
             if (rspLastModified.StatusCode == HttpStatusCode.OK)
             {
@@ -44,29 +89,43 @@ namespace wahdvlib
                     Stream streamResponse = rspLastModified.GetResponseStream();
                     StreamReader reader = new StreamReader(streamResponse, Encoding.GetEncoding(rspLastModified.ContentEncoding));
                     string strLastModified = reader.ReadToEnd();
-                    realm CurrentRealm = JsonConvert.DeserializeObject<realm>(strLastModified);
+                    dumpInfo CurrentRealm = JsonConvert.DeserializeObject<dumpInfo>(strLastModified);
                     if (CurrentRealm.LastModified != lngLastModified)
                     {
                         lngLastModified = CurrentRealm.LastModified;
+                        strAuctionData = CurrentRealm.url;
+                        download(strAuctionData);
                         return true;
                     }
                 }
                 catch (JsonException JsonEx)
                 {
-                	
+                    log.Error(JsonEx.Message);
                 }
                 return false;
             }
             else return false;
         }
 
+        private void download(string auctionDataUrl)
+        {
+            HttpClient downClient = new HttpClient();
+            using (Stream dataStream = downClient.GetStreamAsync(auctionDataUrl).Result)
+            {
+                using (StreamReader streamReader = new StreamReader(dataStream))
+                {
+                    using (JsonReader jsReader = new JsonTextReader(streamReader))
+                    {
+                        JsonSerializer serializer = new JsonSerializer();
+                        currentData = serializer.Deserialize<auctionData>(jsReader);
+                    }
+                }
+            }
+        }
+
     }
 
 
-    public class realm
-    {
-        public UInt64 LastModified { get; set; }
-        public string url { get; set; }
-    }
+
 
 }
