7e3af845917dd27488d3d8b1cc45732e334fcbc6
Ralph <kingiii003@gmail.com>
2013-05-08 14:02:08 +0800

    data access layer modified

--- wahdvlib/dal.cs
+++ wahdvlib/dal.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 using WAHDV.structure;
 
 namespace WAHDV.DAL
@@ -12,47 +10,48 @@ namespace WAHDV.DAL
     public class WAHDataAccessLayer
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger("WAHDV.dao");
-        private SqlConnection connection;
-        private SqlDataAdapter adapter;
-        private SqlCommand command;
-        private SqlCommandBuilder builder;
-        private string connString = "Server=WORKSTATION;Database=AuctionDataDB;User Id=WAHDV_AP;Password=WAHDV_AP;";
-        private string SQL_SELECT_ITEM = "SELECT * FROM dbo.TBL_Moonglade_AuctionItem";
-        private string SQL_SELECT_TRANS = "SELECT * FROM dbo.TBL_Moonglade_Transaction";
-        private string SQL_SELECT_LASTMOD = "SELECT LastModified FROM dbo.TBL_RealmLastModified WHERE RealmName = '";
-        private string SQL_SELECT_CREATEDATE = "SELECT createTime FROM dbo.TBL_Moonglade_AuctionItem WHERE auc = ";
+        private MySqlConnection connection;
+        private MySqlDataAdapter adapter;
+        private MySqlCommand command;
+        private MySqlCommandBuilder builder;
+        private string connString;
+        private string SQL_SELECT_ITEM = "SELECT * FROM auctiondb.TBL_Moonglade_AuctionItem";
+        private string SQL_SELECT_TRANS = "SELECT * FROM auctiondb.TBL_Moonglade_Transaction";
+        private string SQL_SELECT_LASTMOD = "SELECT LastModified FROM auctiondb.TBL_RealmLastModified WHERE RealmName = '";
+        private string SQL_SELECT_CREATEDATE = "SELECT createTime FROM auctiondb.TBL_Moonglade_AuctionItem WHERE auc = ";
 
         public WAHDataAccessLayer()
         {
-            connection = new SqlConnection(connString);
+            connString = ConfigurationManager.ConnectionStrings["CurrentDB"].ConnectionString;
+            connection = new MySqlConnection(connString);
         }
 
         public UInt64 GetLastModified(string RealmName)
         {
-            command = new SqlCommand(SQL_SELECT_LASTMOD + RealmName + "'", GetDBConnection());
+            command = new MySqlCommand(SQL_SELECT_LASTMOD + RealmName + "'", GetDBConnection());
             return Convert.ToUInt64(command.ExecuteScalar());
         }
 
         public DateTime GetCreateDateTime(UInt64 auc)
         {
-            command = new SqlCommand(SQL_SELECT_CREATEDATE + auc, GetDBConnection());
+            command = new MySqlCommand(SQL_SELECT_CREATEDATE + auc, GetDBConnection());
             return Convert.ToDateTime(command.ExecuteScalar());
         }
 
         public bool UpdateLastModified(string RealmName, UInt64 LastModified)
         {
-            string SQL_UPDATE_REALM = "UPDATE dbo.TBL_RealmLastModified SET LastModified = " + LastModified + " , LastUpdate = '" + DateTime.Now.ToString() + "' WHERE RealmName = '" + RealmName + "'";
-            command = new SqlCommand(SQL_UPDATE_REALM, GetDBConnection());
+            string SQL_UPDATE_REALM = "UPDATE auctiondb.TBL_RealmLastModified SET LastModified = " + LastModified + " , LastUpdate = '" + DateTime.Now.ToString() + "' WHERE RealmName = '" + RealmName + "'";
+            command = new MySqlCommand(SQL_UPDATE_REALM, GetDBConnection());
             if (1 == command.ExecuteNonQuery()) return true;
             else return false;
         }
 
         public int SaveNewTransaction(ref DataTable NewTransaction)
         {
-            command = new SqlCommand(SQL_SELECT_TRANS, GetDBConnection());
-            adapter = new SqlDataAdapter();
+            command = new MySqlCommand(SQL_SELECT_TRANS, GetDBConnection());
+            adapter = new MySqlDataAdapter();
             adapter.SelectCommand = command;
-            builder = new SqlCommandBuilder(adapter);
+            builder = new MySqlCommandBuilder(adapter);
             adapter.InsertCommand = builder.GetInsertCommand();
             adapter.ContinueUpdateOnError = true;
             return adapter.Update(NewTransaction);
@@ -60,24 +59,24 @@ namespace WAHDV.DAL
 
         public int SaveNewItems(ref DataTable NewItemTBL)
         {
-            command = new SqlCommand(SQL_SELECT_ITEM, GetDBConnection());
-            adapter = new SqlDataAdapter();
+            command = new MySqlCommand(SQL_SELECT_ITEM, GetDBConnection());
+            adapter = new MySqlDataAdapter();
             adapter.SelectCommand = command;
-            builder = new SqlCommandBuilder(adapter);
+            builder = new MySqlCommandBuilder(adapter);
             adapter.InsertCommand = builder.GetInsertCommand();
             adapter.ContinueUpdateOnError = true;
             return adapter.Update(NewItemTBL);
         }
 
-        public bool SaveNewItem(auctionItem item)
-        {
-            string strInsert = "INSERT INTO dbo.TBL_Moonglade_AuctionItem SET auc = " + item.auc + "";
-            command = new SqlCommand(strInsert, connection);
-            if (command.ExecuteNonQuery() == 1) return true;
-            else return false;
-        }
+        //public bool SaveNewItem(auctionItem item)
+        //{
+        //    string strInsert = "INSERT INTO dbo.TBL_Moonglade_AuctionItem SET auc = " + item.auc + "";
+        //    command = new MySqlCommand(strInsert, connection);
+        //    if (command.ExecuteNonQuery() == 1) return true;
+        //    else return false;
+        //}
 
-        public SqlConnection GetDBConnection()
+        public MySqlConnection GetDBConnection()
         {
             if (connection.State == ConnectionState.Closed)
             {
@@ -85,7 +84,7 @@ namespace WAHDV.DAL
                 {
                     connection.Open();
                 }
-                catch (SqlException sqlEx)
+                catch (MySqlException sqlEx)
                 {
                     log.Error(sqlEx.Message);
                 }
@@ -102,7 +101,7 @@ namespace WAHDV.DAL
                 {
                     connection.Close();
                 }
-                catch (SqlException sqlEx)
+                catch (MySqlException sqlEx)
                 {
                     log.Error(sqlEx.Message);
                 }
